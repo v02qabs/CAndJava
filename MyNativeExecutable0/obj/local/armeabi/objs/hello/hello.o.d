a16f4b24665445b4945f8a2b83528dfb0c0d9216
v02qabs <takesue090@gmail.com>
2019-06-09 11:05:49 +0900

    remove-all

--- MyNativeExecutable0/obj/local/armeabi/objs/hello/hello.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi/objs/hello/hello.o: jni/hello.cpp \
- /data/data/com.aide.ui/no_backup/ndksupport-1710240001/android-ndk-aide/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream
-
-/data/data/com.aide.ui/no_backup/ndksupport-1710240001/android-ndk-aide/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream:
