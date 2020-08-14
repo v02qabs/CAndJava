3d201b09ff24edf5e98b062c898cf35d7cb36c8b
v02qabs <takesue090@gmail.com>
2020-08-14 12:34:41 +0900

    JNIExample-x64

--- GHello/hello.c
+++
@@ -1,7 +0,0 @@
-#include "include.h"
-
-JNIEXPORT void JNICALL Java_Ghello_hellof(JNIEnv *env, jobject obj)
-{
-	printf("Hello.\n");
-}
-
--- GHello/include.h
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-#include <jni.h>
-
-JNIEXPORT void Java_GHello_hellof(JNIEnv *enb, jobject obj);
