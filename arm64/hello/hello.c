83c4ad7bdfde7356213afcb2b98eb1b5ff8b8dc2
v02qabs <takesue090@gmail.com>
2023-11-22 08:33:06 +0900

    hello.c-print-text

--- arm64/hello/hello.c
+++ arm64/hello/hello.c
@@ -4,4 +4,5 @@
 JNIEXPORT void JNICALL Java_MyHello_Hello(JNIEnv *env, jobject obj)
 {
 	printf("Hello.\n");
+	printf("input text: ");
 }
--- arm64/hello/make.sh
+++ arm64/hello/make.sh
@@ -1,4 +1,4 @@
-gcc hello.c -shared -o libhello.so -I /usr/lib/jvm/java-11-openjdk-arm64/include -I /usr/lib/jvm/java-11-openjdk-arm64/include/linux
+gcc hello.c -shared -o libhello.so -I /usr/lib/jvm/java-17-openjdk-arm64/include -I /usr/lib/jvm/java-17-openjdk-arm64/include/linux
 
 javac MyHello.java
 
